d09c0af013a8fb6b7103d80b44fb8db898bb1676
Alexandr <headofthefather@gmail.com>
2023-01-03 13:09:18 +0300

    5 Homework is done

---
+++ 5_HomeWork/Task2/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+void FillRandomArray(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(-100,100);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+int[] numbers = new int[4];
+FillRandomArray(numbers);
+Console.WriteLine("This is your array: ");
+PrintArray(numbers);
+int sum = 0;
+
+for (int i = 0; i < numbers.Length; i = i + 2)
+{
+    sum = sum + numbers[i];
+}
+
+Console.WriteLine($"Sum of elements in odd positions is = {sum}");
---
+++ 5_HomeWork/Task3/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементом массива.
+
+[3.3 7.76 22 2 78.67] -> 76.67
+*/
+void FillRandomArray(double[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+        }
+}
+void PrintArray(double[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write(numbers[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+double[] numbers = new double[4];
+FillRandomArray(numbers);
+Console.WriteLine("This is your array: ");
+PrintArray(numbers);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] > max)
+        {
+            max = numbers[i];
+        }
+    if (numbers[i] < min)
+        {
+            min = numbers[i];
+        }
+}
+
+Console.WriteLine($"Difference between min and max position = {max - min}");
